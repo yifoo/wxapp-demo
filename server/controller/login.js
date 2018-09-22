8a888f98850665996290c78e7dd209aabbce6d97
wuhao <wuhaoxcp@gmail.com>
2018-09-22 23:11:39 +0800

    提交

--- server/controller/login.js
+++ server/controller/login.js
@@ -2,7 +2,7 @@ var request = require('request');
 module.exports = (req, res) => {
   var code = req.query.code
   var option = {
-    url: "https://api.weixin.qq.com/sns/jscode2session?appid=<appid>&secret=<secret>&js_code="+code+"&grant_type=authorization_code",
+    url: "https://api.weixin.qq.com/sns/jscode2session?appid=wxbccade31a8e59d52&secret=c0ed74d03b888ada7528b5eaec84d22b&js_code="+code+"&grant_type=authorization_code",
     method: "GET",
     json: true,
     headers: {
